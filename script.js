11bf1b7f8f81bdcfd1b42043edf24fde494e0de5
NaouiMAHDI <129464369+NaouiMAHDI@users.noreply.github.com>
2023-10-16 19:47:16 +0100

    Update script.js from github

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
 console.log("hello world")
 console.log("Welcome in this cours");
+console.log("Welcome in this cours");
+console.log("Welcome in this cours 1");
+console.log("Welcome in this cours 2");
